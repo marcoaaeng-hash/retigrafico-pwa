f51ecab105e9b263c9a4b2380f14fbfa1e7f67d1
marcoaaeng-hash <marcoaa.eng@gmail.com>
2025-10-18 17:08:48 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,12 @@
 
-const CACHE_NAME = 'retigrafico-cache-v3';
+const CACHE_NAME = 'retigrafico-cache-v32';
 const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
   './icon-512.png',
   './icon-192.png',
-  'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js',
-  'https://cdn.jsdelivr.net/npm/jszip@3.10.1/dist/jszip.min.js',
-  'https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js'
+  'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js'
 ];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
